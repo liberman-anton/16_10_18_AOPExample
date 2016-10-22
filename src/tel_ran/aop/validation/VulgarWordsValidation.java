9192acbf7fa2243084b4413ad4cf17b791bd9668
Anton Libeman <antonliberman@gmail.com>
2016-10-22 13:21:09 +0300

    arrays init in beans

--- src/tel_ran/aop/validation/VulgarWordsValidation.java
+++ src/tel_ran/aop/validation/VulgarWordsValidation.java
@@ -2,19 +2,22 @@ package tel_ran.aop.validation;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 
-import tel_ran.chat.ChatRoom;
-
 public class VulgarWordsValidation{
+	private String[] vul; 
 	
+	public VulgarWordsValidation(String[] vul) {
+		super();
+		this.vul = vul;
+	}
+
 	public Object validate(ProceedingJoinPoint joinPoint) throws Throwable{
 		Object[] args = joinPoint.getArgs();
-		String[] expressions = ChatRoom.getExpressions();
 		if (args != null)
 			for (int i = 0; i < args.length; i++)
 				if(args[i] instanceof String)
-					for(String expression : expressions)
+					for(String expression : vul)
 						args[i] = args[i].toString().replaceAll(
-								"(?i:" + expression + ")", "*****");
+								"(?i)" + expression, "*****");
 		return joinPoint.proceed(args);
 	}
 }
--- src/tel_ran/chat/ChatRoom.java
+++ src/tel_ran/chat/ChatRoom.java
@@ -1,19 +1,11 @@
 package tel_ran.chat;
 
 public class ChatRoom {
-	private static String[] expressions;
-	
-	public static String[] getExpressions() {
-		return expressions;
-	}
-	public ChatRoom(String[] expressions) {
-		super();
-		this.expressions = expressions;
-	}
 	public void saySomething(String message){
 		System.out.println(message + " has been said");
 	}
-	public void replySomething(String message){
+	public void replySomething(String message) throws InterruptedException{
 		System.out.println(message + " has been said replied");
+		Thread.sleep(1000);
 	}
 }
